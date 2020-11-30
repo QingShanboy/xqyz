861993c0a5ed14f50a55f3e9a25e4ac4e90e4c47
yb <yangbo101131@163.com>
2020-11-30 21:11:28 +0800

    aixos封装

---
+++ src/utils/axios.js
@@ -0,0 +1,59 @@
+import axios from 'axios'
+
+class HttpRequest {
+  constructor (baseUrl) {
+    this.baseUrl = baseUrl
+  }
+
+  // 获取axios配置
+  getInsideConfig () {
+    const config = {
+      baseURL: this.baseUrl,
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      timeout: 10000
+    }
+    return config
+  }
+
+  // 去设定拦截器
+  interceptors (instance) {
+    instance.interceptors.request.use(function (config) {
+      // 在发送请求之前做些什么
+      return config
+    }, function (error) {
+      // 对请求错误做些什么
+      return Promise.reject(error)
+    })
+    // 添加响应拦截器
+    instance.interceptors.response.use(res => {
+      // 对响应数据做点什么
+      if (res.status === 200) {
+        return Promise.resolve(res.data)
+      } else {
+        return Promise.resolve(res.data)
+      }
+    }, function (error) {
+      // 对响应错误做点什么
+      return Promise.reject(error)
+    })
+  }
+
+  // 创建实例
+  request (options) {
+    const instance = axios.create()
+    const newOptions = Object.assign(this.getInsideConfig(), options)
+    this.interceptors(instance)
+    return instance.request(newOptions)
+  }
+}
+// const instance = axios.create({
+//   baseURL: process.env.NODE_ENV === 'development' ? config.baseUrl.dev : config.baseUrl.pro,
+//   headers: {
+//     'Content-Type': 'application/json;charset=utf-8'
+//   },
+//   timeout: 10000
+// })
+
+export default HttpRequest
